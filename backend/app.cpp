ffb0e0e19d44e302670c3f13d3664fac9521c026
pes2ug23cs918-harshita <harsh@CachePotato.>
2026-02-26 07:39:20 +0000

    corrected

--- backend/app.cpp
+++ backend/app.cpp
@@ -4,13 +4,20 @@
 #include <netinet/in.h>
 
 int main() {
+    char hostname[256];
+    gethostname(hostname, sizeof(hostname));
+
     int server_fd, new_socket;
     struct sockaddr_in address;
     int opt = 1;
     int addrlen = sizeof(address);
 
-    const char* response =
-        "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nServed by backend container\n";
+    std::string body = "Served by container: ";
+    body += hostname;
+    body += "\n";
+
+    std::string response =
+        "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\n" + body;
 
     server_fd = socket(AF_INET, SOCK_STREAM, 0);
     setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
@@ -26,7 +33,7 @@ int main() {
 
     while (true) {
         new_socket = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen);
-        send(new_socket, response, strlen(response), 0);
+        send(new_socket, response.c_str(), response.size(), 0);
         close(new_socket);
     }
 
